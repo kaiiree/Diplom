6873888affe6cb0c0b326110577dbbe871b1f993
Kayrat <kkunceraev@gmail.com>
2024-09-05 14:05:11 +0500

    first commit

---
+++ app/database_files/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Integer, default=1)
+
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    price = Column(Integer)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User")
